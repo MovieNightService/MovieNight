f45a98367b4110067d582afa3fc388d6eb1ec5a7
oleksandr.kibkalo <oleksandr.kibkalo@chisw.com>
2020-11-14 10:06:55 +0200

    implemented ChatService & split architecture to microservices

---
+++ mn-chat/src/main/java/com/movienight/chat/MovieNightChatApplication.java
@@ -0,0 +1,12 @@
+package com.movienight.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieNightChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieNightChatApplication.class, args);
+	}
+}
---
+++ mn-chat/src/main/java/com/movienight/chat/persistence/MessageType.java
@@ -0,0 +1,7 @@
+package com.movienight.chat.persistence;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
